0bac66f98fc674af9aca4e558ef24d41e203a61d
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-11 00:15:58 +0530

    Hero section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,24 @@
+const Hero = () => {
+    return (
+        <>
+            <div className="section hero-section" id="hero">
+                <div className="hero-main-content">
+                    <h1 className="hero-main-txt">
+                        WELCOME TO <span>TECHERA</span> SOLUTIONS
+                    </h1>
+                    <h1 className="hero-main-txt">
+                        END TO END <span>NFT</span> COLLECTION DEVELOPMENT
+                    </h1>
+                </div>
+                <div className="hero-sub-content">
+                    <h3 className="hero-sub-txt">
+                        READY TO <span>GET</span> STARTED!
+                    </h3>
+                    <a href="#" className="main-btn">Get Started</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero
