9903c90f9d1bcc78a3e9883193b3b2ba3c78d6a7
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-11 02:24:50 +0530

    Background particle animation added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
+import ParticlesBg from "./Components/ParticlesBg";
 
 const App = () => {
   return (
     <>
+      <ParticlesBg />
       <NavBar />
       <Hero />
     </>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/ParticlesBg.js
@@ -0,0 +1,12 @@
+import Particles from "react-tsparticles"
+import ParticlesConfig from "../config/particle-config"
+
+const ParticlesBg = () => {
+    return (
+        <>
+            <Particles params={ParticlesConfig}></Particles>
+        </>
+    )
+}
+
+export default ParticlesBg
