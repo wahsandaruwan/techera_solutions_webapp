ca4adead508abb3cd6fe0e08aca812ab9ff0fd7b
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-13 00:48:59 +0530

    code updated

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -2,10 +2,10 @@ const NavMenu = () => {
     return (
         <>
             <div className="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">FAQ</a></li>
+                <li><a href="home">Home</a></li>
+                <li><a href="services">Services</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="faq">FAQ</a></li>
             </div>
         </>
     )
